b62cce017065ea35471cd594f6d3a4fcb7919008
AJAY KUMAR SARASWAT <ajaykumarsaraswat9045@gmail.com>
2024-12-26 14:54:30 +0530

    setup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
+app.use(express.json());
 app.listen(PORT, (req, res) => {
 	console.log(`server is running on PORT ${PORT}`);
 });
